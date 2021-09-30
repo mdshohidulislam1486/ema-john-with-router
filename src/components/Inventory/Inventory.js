5218172925a19de38912f155898aa43e69084e6b
mdshohidulislam1486 <Shohidul Islam>
2021-09-30 21:29:57 +0600

    updated

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h2>This is our inventory</h2>
+            <h2>Updated the inventory page</h2>
+            
+        </div>
+    );
+};
+
+export default Inventory;
